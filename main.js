cfc7caa7d66ead2ed9af64af07be77aa0dc22cd3
Annika Pomeroy <annikapomeroy@gmail.com>
2025-06-30 21:36:40 +0000

    Basic external user setup

---
+++ main.js
@@ -0,0 +1,27 @@
+import { LitElement, html, css } from 'lit';
+import 'katapult-pro-extended/src/elements/page-element/katapult-page-element.js';
+
+export class MainPage extends LitElement {
+  static properties = {}
+  static styles = css`
+    :host {
+      /* Ideally the primary and secondary color should compliment each other or be different tones of the same color */
+      --primary-color: var(--sl-color-indigo-700, darkgrey) !important; /* Middle Color */
+      --secondary-color: var(--sl-color-indigo-950, black) !important; /* Darkest Color */
+      --accent-color: var(--sl-color-amber-400, orange) !important; /* Bright/Standout Color */
+    }
+  `
+  render() {
+    return html`
+      <!-- Page Element Content -->
+      <katapult-page-element .supportNum=${'111-111-1111'} .supportEmail=${'email@company.com'} .companyName=${'Company Name'} .logoLink=${''}>
+        <!-- <katapult-job-picker></katapult-job-picker> -->
+        <!-- <katapult-warning>Alert</katapult-warning> -->
+      </katapult-page-element>
+    `
+  }
+  constructor() {
+    super();
+  }
+}
+window.customElements.define('main-page', MainPage);
